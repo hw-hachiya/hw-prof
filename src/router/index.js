b56bf53d98750adc81a21fa496c47f0668b03984
t-hachiya <hw.hachiya@gmail.com>
2023-02-12 13:34:55 +0900

    相対パスに変更した

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,19 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "./",
     name: "home",
     component: HomeView,
     meta: { title: "HW Home", desc: "This is HW Home" },
   },
   {
-    path: "/works",
+    path: "works",
     name: "works",
     component: () => import("../views/WorksView.vue"),
     meta: { title: "Works 実績", desc: "実績をご紹介します。" },
   },
   {
-    path: "/about",
+    path: "about",
     name: "about",
     component: () => import("../views/AboutView.vue"),
     meta: { title: "About このサイトについて", desc: "HWとは、連絡先など..." },
